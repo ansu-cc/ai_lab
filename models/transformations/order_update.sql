531d1dbec1361977d82a95e6798b1dbe649b3838
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-25 16:18:07 +0530

    Update order_update.sql

--- models/transformations/order_update.sql
+++ models/transformations/order_update.sql
@@ -1,6 +1,14 @@
-WITH date_shift AS (
-    SELECT CURRENT_DATE - MAX(orderTImestamp) AS shift_days
-    FROM webshop.order
-)
-UPDATE webshop.order
-SET orderTImestamp = orderTImestamp + (SELECT shift_days FROM date_shift);
+{{ config(
+    materialized='incremental',
+    unique_key='order_id'
+) }}
+
+SELECT
+    order_id,
+    orderTimestamp + (CURRENT_DATE - MAX(orderTimestamp) OVER()) AS orderTimestamp
+FROM webshop.order
+
+{% if is_incremental() %}
+WHERE orderTimestamp > (SELECT MAX(orderTimestamp) FROM {{ this }})
+{% endif %}
+
