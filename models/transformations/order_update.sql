f89fa769138c6d87fa4e070dfbcb180703afec40
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-25 16:37:58 +0530

    Update order_update.sql

--- models/transformations/order_update.sql
+++ models/transformations/order_update.sql
@@ -3,10 +3,15 @@
     unique_key='id'
 ) }}
 
+WITH latest_date AS (
+    -- Get the latest ordertimestamp from the existing table
+    SELECT MAX(ordertimestamp) AS last_order_date FROM {{ this }}
+)
+
 SELECT
     id,
     customerid,
-    ordertimestamp + (CURRENT_DATE - MAX(ordertimestamp) OVER()) AS ordertimestamp,
+    ordertimestamp + (SELECT CURRENT_DATE - last_order_date FROM latest_date) AS ordertimestamp,
     shippingaddressid,
     total,
     shippingcost,
@@ -17,4 +22,3 @@ FROM webshop.order
 {% if is_incremental() %}
 WHERE ordertimestamp > (SELECT MAX(ordertimestamp) FROM {{ this }})
 {% endif %}
-
