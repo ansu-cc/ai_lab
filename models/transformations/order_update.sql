5af5cec8d6e77a5200456b19625a5ea07d0d334b
ansu-cc <aannavarghese@ecloudcontrol.com>
2025-02-25 16:13:17 +0530

    Update order_update.sql

--- models/transformations/order_update.sql
+++ models/transformations/order_update.sql
@@ -1,2 +1,6 @@
+WITH date_shift AS (
+    SELECT CURRENT_DATE - MAX(orderTImestamp) AS shift_days
+    FROM webshop.order
+)
 UPDATE webshop.order
-SET created = CURRENT_TIMESTAMP, updated = CURRENT_TIMESTAMP;
+SET orderTImestamp = orderTImestamp + (SELECT shift_days FROM date_shift);
